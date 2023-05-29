38603d14f5990e14bfd28051b85584b46f510d7b
Shug Ghosh <shug@alphaexplorationco.com>
2023-05-29 16:00:52 -0700

    Add frontend template

---
+++ cmt-frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="bg-blue-500 p-5">
+      <h1 className="text-3xl text-white">Hello, Tailwind CSS!</h1>
+    </div>
+  )
+}
