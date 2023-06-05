7249d2312a2219de36533e25088ff24513507e7f
Shug Ghosh <shug@alphaexplorationco.com>
2023-06-04 22:07:05 -0700

    Update page margins

--- cmt-frontend/app/page.tsx
+++ cmt-frontend/app/page.tsx
@@ -7,13 +7,13 @@ export default function ContentReview() {
   return (
     <div>
       <div className="flex bg-white border border-gray-300 justify-end items-center w-full h-16">
-        <p className="w-fit h-7 text-xl font-medium text-gray-500 px-4">
+        <p className="w-fit h-7 text-xl font-medium text-gray-500 px-4 mx-2">
           <a className="mx-2 lg:mx-4 hover:text-black" href="http://www.google.com">All Reviews</a>
           <a className="mx-2 lg:mx-4 hover:text-black" href="http://www.google.com">Previous</a>
           <a className="mx-2 lg:mx-4 hover:text-black" href="http://www.google.com">Next</a>
         </p>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:mx-8">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mx-1 lg:mx-8">
         <div className="col-span-1">
           <ContentCard
             contentType="image"
