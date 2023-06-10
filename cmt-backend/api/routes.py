e6be4632c4fb7040a15130404c7305aa9ba2e99f
Shug Ghosh <shug@lendtable.com>
2023-06-10 10:07:50 -0700

    Add queue routes

--- cmt-backend/api/routes.py
+++ cmt-backend/api/routes.py
@@ -1,35 +1,65 @@
 from django.http import JsonResponse, HttpResponse, HttpRequest, HttpResponseNotAllowed
-from api.models import QueueCMT, DecisionTreeCMT
+from api.models import QueueCMT, DecisionTreeCMT, ReviewCMT
 
 
-def get_queues(request: HttpRequest) -> JsonResponse:
-    if request.method != "GET":
-        return HttpResponseNotAllowed(["GET"])
+class QueueRoutes:
+    @staticmethod
+    def get_queues(request: HttpRequest) -> JsonResponse:
+        if request.method != "GET":
+            return HttpResponseNotAllowed(["GET"])
 
-    queues = QueueCMT.objects.all()
-    queue_array = []
-    for q in queues:
-        queue_array.append({"id": q.id, "name": q.name})
+        queues = QueueCMT.objects.all().order_by("name")
+        queue_array = [{"id": q.id, "name": q.name} for q in queues]
 
-    response = {"queues": queue_array}
+        return JsonResponse({"queues": queue_array})
 
-    return JsonResponse(response)
+    @staticmethod
+    def create_queue(request: HttpRequest) -> HttpResponse:
+        if request.method != "POST":
+            return HttpResponseNotAllowed(["POST"])
 
+        queue = QueueCMT(
+            name=request.POST["name"],
+            decision_tree_id=request.POST["decision_tree_id"]
+            if "decision_tree_id" in request.POST
+            else None,
+            prioritization_function=request.POST["prioritization_function"]
+            if "prioritization_function" in request.POST
+            else None,
+        )
 
-def create_queue(request: HttpRequest) -> HttpResponse:
-    if request.method != "POST":
-        return HttpResponseNotAllowed(["POST"])
+        queue.save()
 
-    queue = QueueCMT(
-        name=request.POST["name"],
-        decision_tree=DecisionTreeCMT.objects.get(id=request.POST["decision_tree_id"])
-        if "decision_tree_id" in request.POST
-        else None,
-        prioritization_function=request.POST["prioritization_function"]
-        if "prioritization_function" in request.POST
-        else None,
-    )
+        return HttpResponse("OK")
 
-    queue.save()
+    @staticmethod
+    def modify_queue(request: HttpRequest, queue_id: int) -> HttpResponse:
+        if request.method != "POST":
+            return HttpResponseNotAllowed(["POST"])
 
-    return HttpResponse("OK")
+        queue = QueueCMT.objects.get(id=queue_id)
+        if request.POST.get("name"):
+            queue.name = request.POST.get("name")
+
+        if request.POST.get("decision_tree_id"):
+            queue.decision_tree_id = request.POST.get("decision_tree_id")
+
+        if request.POST.get("prioritization_function"):
+            queue.prioritization_function = request.POST.get("prioritization_function")
+
+        queue.save()
+
+        return HttpResponse("OK")
+
+    @staticmethod
+    def get_reviews_for_queue(request: HttpRequest, queue_id: int) -> JsonResponse:
+        if request.method != "GET":
+            return HttpResponseNotAllowed(["GET"])
+
+        reviews = ReviewCMT.objects.filter(queue_id=queue_id).order_by("create_time")
+        review_array = [
+            {"id": r.id, "entity_id": r.entity_id, "entity_type": r.entity_type}
+            for r in reviews
+        ]
+
+        return JsonResponse({"reviews": review_array})
--- cmt-backend/api/urls.py
+++ cmt-backend/api/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import routes
 
 urlpatterns = [
-    path("queues/", routes.get_queues),
-    path("create-queue/", routes.create_queue),
+    path("queues/", routes.QueueRoutes.get_queues),
+    path("create-queue/", routes.QueueRoutes.create_queue),
+    path("modify-queue/<int:queue_id>/", routes.QueueRoutes.modify_queue),
+    path("queue/<int:queue_id>/", routes.QueueRoutes.get_reviews_for_queue),
 ]
