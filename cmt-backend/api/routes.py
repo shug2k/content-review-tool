24659ec88e9d0ff8e40ae14ce9bf7548cd6ba2b8
Shug Ghosh <shug@lendtable.com>
2023-06-10 11:34:06 -0700

    Add remaining routes

--- cmt-backend/api/routes.py
+++ cmt-backend/api/routes.py
@@ -20,12 +20,8 @@ class QueueRoutes:
 
         queue = QueueCMT(
             name=request.POST["name"],
-            decision_tree_id=request.POST["decision_tree_id"]
-            if "decision_tree_id" in request.POST
-            else None,
-            prioritization_function=request.POST["prioritization_function"]
-            if "prioritization_function" in request.POST
-            else None,
+            decision_tree_id=request.POST.get("decision_tree_id"),
+            prioritization_function=request.POST.get("prioritization_function"),
         )
 
         queue.save()
@@ -38,13 +34,12 @@ class QueueRoutes:
             return HttpResponseNotAllowed(["POST"])
 
         queue = QueueCMT.objects.get(id=queue_id)
-        if request.POST.get("name"):
+
+        if request.POST.get("name") is not None:
             queue.name = request.POST.get("name")
-
-        if request.POST.get("decision_tree_id"):
+        if request.POST.get("decision_tree_id") is not None:
             queue.decision_tree_id = request.POST.get("decision_tree_id")
-
-        if request.POST.get("prioritization_function"):
+        if request.POST.get("prioritization_function") is not None:
             queue.prioritization_function = request.POST.get("prioritization_function")
 
         queue.save()
@@ -74,3 +69,173 @@ class QueueRoutes:
         ]
 
         return JsonResponse({"reviews": review_array})
+
+
+class ReviewRoutes:
+    @staticmethod
+    def get_review_response(review: ReviewCMT) -> JsonResponse:
+        return JsonResponse(
+            {
+                "entity_id": review.entity_id,
+                "entity_type": review.entity_type,
+                "entity_create_time": review.entity_create_time,
+                "entity_metadata": review.entity_metadata,
+                "user_id": review.user_id,
+                "user_name": review.user_name,
+                "user_email": review.user_email,
+                "user_phone_number": review.user_phone_number,
+                "user_metadata": review.user_metadata,
+            }
+        )
+
+    @staticmethod
+    def get_review(request: HttpRequest, review_id: int) -> JsonResponse:
+        if request.method != "GET":
+            return HttpResponseNotAllowed(["GET"])
+
+        review = ReviewCMT.objects.get(id=review_id)
+
+        return ReviewRoutes.get_review_obj(review)
+
+    @staticmethod
+    def get_next_review(request: HttpRequest, review_id: int) -> JsonResponse:
+        if request.method != "GET":
+            return HttpResponseNotAllowed(["GET"])
+
+        curr_review = ReviewCMT.objects.get(id=review_id)
+
+        next_review = (
+            ReviewCMT.objects.filter(
+                queue_id=curr_review.queue.id, create_time__gt=curr_review.create_time
+            )
+            .order_by("create_time")
+            .first()
+        )
+
+        return ReviewRoutes.get_review_response(next_review)
+
+    @staticmethod
+    def get_prev_review(request: HttpRequest, review_id: int) -> JsonResponse:
+        if request.method != "GET":
+            return HttpResponseNotAllowed(["GET"])
+
+        curr_review = ReviewCMT.objects.get(id=review_id)
+
+        prev_review = (
+            ReviewCMT.objects.filter(
+                queue_id=curr_review.queue.id, create_time__lt=curr_review.create_time
+            )
+            .order_by("create_time")
+            .last()
+        )
+
+        return ReviewRoutes.get_review_response(prev_review)
+
+    @staticmethod
+    def create_review(request: HttpRequest) -> HttpResponse:
+        if request.method != "POST":
+            return HttpResponseNotAllowed(["POST"])
+
+        review = ReviewCMT(
+            entity_id=request.POST.get("entity_id"),
+            entity_type=request.POST.get("entity_type"),
+            entity_create_time=request.POST.get("entity_create_time"),
+            entity_metadata=request.POST.get("entity_metadata"),
+            user_id=request.POST.get("user_id"),
+            user_name=request.POST.get("user_name"),
+            user_email=request.POST.get("user_email"),
+            user_phone_number=request.POST.get("user_phone_number"),
+            user_metadata=request.POST.get("user_metadata"),
+        )
+
+        review.save()
+
+        return HttpResponse("OK")
+
+    @staticmethod
+    def modify_review(request: HttpRequest, review_id: int) -> HttpResponse:
+        if request.method != "POST":
+            return HttpResponseNotAllowed(["POST"])
+
+        review = ReviewCMT.objects.get(id=review_id)
+
+        if request.POST.get("entity_id") is not None:
+            review.entity_id = request.POST.get("entity_id")
+        if request.POST.get("entity_type") is not None:
+            review.entity_type = request.POST.get("entity_type")
+        if request.POST.get("entity_create_time") is not None:
+            review.entity_create_time = request.POST.get("entity_create_time")
+        if request.POST.get("entity_metadata") is not None:
+            review.entity_metadata = request.POST.get("entity_metadata")
+        if request.POST.get("user_id") is not None:
+            review.user_id = request.POST.get("user_id")
+        if request.POST.get("user_name") is not None:
+            review.user_name = request.POST.get("user_name")
+        if request.POST.get("user_email") is not None:
+            review.user_email = request.POST.get("user_email")
+        if request.POST.get("user_phone_number") is not None:
+            review.user_phone_number = request.POST.get("user_phone_number")
+        if request.POST.get("user_metadata") is not None:
+            review.user_metadata = request.POST.get("user_metadata")
+
+        review.save()
+
+        return HttpResponse("OK")
+
+    @staticmethod
+    def delete_review(request: HttpRequest, review_id: int) -> HttpResponse:
+        if request.method != "POST":
+            return HttpResponseNotAllowed(["POST"])
+
+        review = ReviewCMT.objects.get(id=review_id)
+
+        review.delete()
+
+        return HttpResponse("OK")
+
+
+class DecisionTreeRoutes:
+    @staticmethod
+    def create_decision_tree(request: HttpRequest) -> HttpResponse:
+        if request.method != "POST":
+            return HttpResponseNotAllowed(["POST"])
+
+        decision_tree = DecisionTreeCMT(
+            name=request.POST["name"],
+            tree=request.POST.get("tree"),
+        )
+
+        decision_tree.save()
+
+        return HttpResponse("OK")
+
+    @staticmethod
+    def modify_decision_tree(
+        request: HttpRequest, decision_tree_id: int
+    ) -> HttpResponse:
+        if request.method != "POST":
+            return HttpResponseNotAllowed(["POST"])
+
+        decision_tree = DecisionTreeCMT.objects.get(id=decision_tree_id)
+
+        if request.POST.get("name") is not None:
+            decision_tree.name = request.POST.get("name")
+        if request.POST.get("decision_tree") is not None:
+            decision_tree.decision_tree = request.POST.get("decision_tree")
+
+        decision_tree.save()
+
+        return HttpResponse("OK")
+
+    @staticmethod
+    def delete_decision_tree(
+        request: HttpRequest, decision_tree_id: int
+    ) -> HttpResponse:
+        if request.method != "POST":
+            return HttpResponseNotAllowed(["POST"])
+
+        decision_tree = DecisionTreeCMT.objects.get(id=decision_tree_id)
+
+        decision_tree.delete()
+
+        return HttpResponse("OK")
--- cmt-backend/api/urls.py
+++ cmt-backend/api/urls.py
@@ -3,9 +3,25 @@ from django.urls import path
 from . import routes
 
 urlpatterns = [
+    # Queues
     path("queues/", routes.QueueRoutes.get_queues),
     path("create-queue/", routes.QueueRoutes.create_queue),
-    path("modify-queue/<int:queue_id>/", routes.QueueRoutes.modify_queue),
-    path("delete-queue/<int:queue_id>/", routes.QueueRoutes.delete_queue),
-    path("queue/<int:queue_id>/", routes.QueueRoutes.get_reviews_for_queue),
+    path("modify-queue/<int:queue_id>", routes.QueueRoutes.modify_queue),
+    path("delete-queue/<int:queue_id>", routes.QueueRoutes.delete_queue),
+    path("queue/<int:queue_id>", routes.QueueRoutes.get_reviews_for_queue),
+    # Reviews
+    path("review/<int:review_id>", routes.ReviewRoutes.get_review),
+    path("create-review/", routes.ReviewRoutes.create_review),
+    path("modify-review/<int:review_id>", routes.ReviewRoutes.modify_review),
+    path("delete-review/<int:review_id>", routes.ReviewRoutes.delete_review),
+    # Decision Trees
+    path("create-decision-tree/", routes.DecisionTreeRoutes.create_decision_tree),
+    path(
+        "modify-decision-tree/<int:decision_tree_id>",
+        routes.DecisionTreeRoutes.modify_decision_tree,
+    ),
+    path(
+        "delete-decision-tree/<int:decision_tree_id>",
+        routes.DecisionTreeRoutes.delete_decision_tree,
+    ),
 ]
