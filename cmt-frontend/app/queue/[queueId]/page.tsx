5a9999e0430c4d7b66e0f565d3dd3b37c82b1843
Shug Ghosh <shug@lendtable.com>
2023-06-22 14:57:38 -0700

    Add page for reviews for a queue

--- cmt-frontend/app/queue/[queueId]/page.tsx
+++ cmt-frontend/app/queue/[queueId]/page.tsx
@@ -1,6 +1,6 @@
 
-async function getData() {
-  const res = await fetch('http://localhost:8000/queues');
+async function getData(queueId: string) {
+  const res = await fetch('http://localhost:8000/queue/' + queueId);
  
   if (!res.ok) {
     throw new Error('Failed to fetch data');
@@ -9,31 +9,35 @@ async function getData() {
   return res.json();
 }
 
-export default async function Queues() {
-  const data = await getData();
+export default async function Reviews({ 
+  params: { queueId },
+}: {
+  params: { queueId: string }
+}) {
+  const data = await getData(queueId);
 
   return (
     <div>
       <div className="flex bg-white border border-gray-300 items-center max-sm:justify-center w-full h-16">
         <p className="w-fit h-7 text-xl font-medium text-gray-500 px-4 md:mx-2">
-          <span className="mx-3 lg:mx-4 hover:text-black">Queues</span>
+          <span className="mx-3 lg:mx-4 hover:text-black">Reviews</span>
         </p>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mx-1 lg:mx-8 py-4">
         <table className="table-fixed w-full mx-auto border">
           <thead className="bg-gray-200">
             <tr>
-              <th className="text-left font-normal border">NAME</th>
-              <th className="text-left font-normal border">COUNT</th>
+              <th className="text-left font-normal border">ID</th>
+              <th className="text-left font-normal border">Type</th>
             </tr>
           </thead>
           <tbody>
-            {data.queues.map((item: {name: string, item_count: string}) => {
+            {data.queues.map((item: {entity_id: string, entity_type: string}) => {
               {console.log(item)}
               return (
                 <tr>
-                  <td className="text-left font-bold hover:bg-gray-300 border">{item.name}</td>
-                  <td className="text-left border">{item.item_count}</td>
+                  <td className="text-left font-bold hover:bg-gray-300 border">{item.entity_id}</td>
+                  <td className="text-left border">{item.entity_type}</td>
                 </tr>
               );
             })}
