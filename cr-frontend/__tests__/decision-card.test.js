29a06530767e0bb5ebab33524876cccbe5566734
Shug Ghosh <shug@lendtable.com>
2023-07-13 11:31:40 -0700

    Fix mock

--- cr-frontend/__tests__/decision-card.test.js
+++ cr-frontend/__tests__/decision-card.test.js
@@ -4,9 +4,23 @@ import mockRouter from 'next-router-mock';
 import { act } from 'react-dom/test-utils';
 
 import DecisionCard from '../app/review/components/decision-card';
-import * as decisions from '../app/review/utils/decisions';
+//import * as decisions from '../app/review/utils/decisions';
 
-jest.mock('../app/review/utils/decisions');
+jest.mock('../app/review/utils/decisions', () => {
+  // Require the original module to not be mocked...
+  const originalModule = jest.requireActual('../app/review/utils/decisions');
+
+  return {
+    handleBackClick: originalModule.handleBackClick,
+    handleNextClick: originalModule.handleNextClick,
+    handleSubmitClick: jest.fn((reviewId, questionsWithAnswers, currentQuestion, currentAnswer) => {
+      return;
+    }),
+  };
+});
+
+const handleSubmitClick = require('../app/review/utils/decisions').handleSubmitClick;
+
 
 const yesAnswer = {
     tag: "yes",
@@ -72,7 +86,7 @@ describe('Decision card rendering', () => {
     const submit = screen.getByText("Submit");
     act(() => {
       fireEvent.click(submit);
-      expect(decisions.handleSubmitClick).toHaveBeenCalledWith(
+      expect(handleSubmitClick).toHaveBeenCalledWith(
         "1",
         [],
         violatingQuestion,
