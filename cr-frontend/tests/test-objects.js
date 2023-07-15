507896521b132300f7edee2582fc48c6862e88c2
Shug Ghosh <shug@alphaexplorationco.com>
2023-07-15 13:27:16 -0700

    Add more frontend tests

--- cr-frontend/tests/test-objects.js
+++ cr-frontend/tests/test-objects.js
@@ -9,13 +9,36 @@ export const noAnswer = {
     text: "No",
 }
 
+export const yesAnswerEscalate = {
+    tag: "yes",
+    text: "Yes",
+    next_question_tag: "escalate",
+}
+
 export const violatingQuestion = {
     tag: "is_violating",
     text: "Is this content violating?",
     answers: [yesAnswer, noAnswer],
 }
 
+export const violatingQuestionWithEscalate = {
+    tag: "is_violating_esc",
+    text: "Is this content violating?",
+    answers: [yesAnswerEscalate, noAnswer],
+}
+
+export const escalationQuestion = {
+    tag: "escalate",
+    text: "Should this content be escalated?",
+    answers: [yesAnswer, noAnswer],
+}
+
 export const basicDecisionTree = {
     start_question_tag: "is_violating",
     questions: [violatingQuestion],
+}
+
+export const escDecisionTree = {
+    start_question_tag: "is_violating_esc",
+    questions: [violatingQuestionWithEscalate, escalationQuestion],
 }
--- cr-frontend/tests/utils.test.js
+++ cr-frontend/tests/utils.test.js
@@ -1,26 +1,65 @@
 import { handleSubmitClick, handleBackClick, handleNextClick } from "../app/review/utils/decisions";
-import { yesAnswer, noAnswer, violatingQuestion, basicDecisionTree } from "./test-objects";
+import {
+    yesAnswer,
+    noAnswer,
+    violatingQuestion,
+    basicDecisionTree,
+    escDecisionTree,
+    yesAnswerEscalate,
+    violatingQuestionWithEscalate, 
+    escalationQuestion
+} from "./test-objects";
 
 global.fetch = jest.fn(() => Promise.resolve("OK"));
 
 describe('submit click handler', () => {
-    it('basic update', () => {
-        handleSubmitClick(
+    it('basic submission call', async () => {
+        await handleSubmitClick(
             "1",
             [],
             violatingQuestion,
-            yesAnswer
+            noAnswer
         );
+
+        expect(fetch).toBeCalled();
     });
 });
 
-/*describe('next click handler', () => {
-    it('basic update', () => {
-        expect(handleNextClick(
+describe('next click handler', () => {
+    it('basic call', () => {
+        const { updatedQuestionsWithAnswers, nextQuestion } = handleNextClick(
             [],
-            basicDecisionTree,
-            violatingQuestion,
-            yesAnswer
-        )).toThrow()
+            escDecisionTree,
+            violatingQuestionWithEscalate,
+            yesAnswerEscalate
+        );
+
+        expect(updatedQuestionsWithAnswers).toStrictEqual([{ 
+            questionTag: violatingQuestionWithEscalate.tag,  
+            answerTag: yesAnswerEscalate.tag
+        }]);
+        expect(nextQuestion).toStrictEqual(escalationQuestion);
     })
-});*/
+
+    it('no next_question_tag - should throw', () => {
+        expect(() => {
+            handleNextClick(
+                [],
+                basicDecisionTree,
+                violatingQuestion,
+                yesAnswer
+            )}
+        ).toThrow()
+    })
+
+    it('next_question_tag does not exist in tree - should throw', () => {
+        expect(() => {
+            handleNextClick(
+                [],
+                basicDecisionTree,
+                violatingQuestionWithEscalate,
+                yesAnswerEscalate
+            )}
+        ).toThrow()
+    })
+});
