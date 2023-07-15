bc9a8b8d4b769c01bb33fd0b3778dbf749dcbcca
Shug Ghosh <shug@alphaexplorationco.com>
2023-07-15 16:46:17 -0700

    Add back tests

--- cr-frontend/tests/test-objects.js
+++ cr-frontend/tests/test-objects.js
@@ -15,6 +15,24 @@ export const yesAnswerEscalate = {
     next_question_tag: "escalate",
 }
 
+export const yesPastViolator = {
+    tag: "yes",
+    text: "Yes",
+    next_question_tag: "is_violating_esc",
+}
+
+export const noPastViolator = {
+    tag: "no",
+    text: "No",
+    next_question_tag: "is_violating_esc",
+}
+
+export const userPastViolatorQuestion = {
+    tag: "is_past_violator",
+    text: "Has this user violated in the past?",
+    answers: [yesPastViolator, noPastViolator],
+}
+
 export const violatingQuestion = {
     tag: "is_violating",
     text: "Is this content violating?",
@@ -41,4 +59,13 @@ export const basicDecisionTree = {
 export const escDecisionTree = {
     start_question_tag: "is_violating_esc",
     questions: [violatingQuestionWithEscalate, escalationQuestion],
+}
+
+export const pastViolatorDecisionTree = {
+    start_question_tag: "is_past_violator",
+    questions: [
+        violatingQuestionWithEscalate,
+        escalationQuestion,
+        userPastViolatorQuestion
+    ],
 }
--- cr-frontend/tests/utils.test.js
+++ cr-frontend/tests/utils.test.js
@@ -1,13 +1,16 @@
 import { handleSubmitClick, handleBackClick, handleNextClick } from "../app/review/utils/decisions";
 import {
-    yesAnswer,
     noAnswer,
+    noPastViolator,
+    yesAnswer,
+    yesAnswerEscalate,
+    escalationQuestion,
+    userPastViolatorQuestion,
     violatingQuestion,
+    violatingQuestionWithEscalate, 
     basicDecisionTree,
     escDecisionTree,
-    yesAnswerEscalate,
-    violatingQuestionWithEscalate, 
-    escalationQuestion
+    pastViolatorDecisionTree,
 } from "./test-objects";
 
 global.fetch = jest.fn(() => Promise.resolve("OK"));
@@ -18,6 +21,17 @@ describe('submit click handler', () => {
             "1",
             [],
             violatingQuestion,
+            noAnswer
+        );
+
+        expect(fetch).toBeCalled();
+    });
+
+    it('submission call with multiple answers', async () => {
+        await handleSubmitClick(
+            "15",
+            [{ questionTag: violatingQuestionWithEscalate.tag, answerTag: yesAnswerEscalate.tag }],
+            escalationQuestion,
             noAnswer
         );
 
@@ -41,6 +55,27 @@ describe('next click handler', () => {
         expect(nextQuestion).toStrictEqual(escalationQuestion);
     })
 
+    it('includes previous questions', () => {
+        const { updatedQuestionsWithAnswers, nextQuestion } = handleNextClick(
+            [{ questionTag: userPastViolatorQuestion.tag, answerTag: noPastViolator.tag }],
+            pastViolatorDecisionTree,
+            violatingQuestionWithEscalate,
+            yesAnswerEscalate
+        );
+
+        expect(updatedQuestionsWithAnswers).toStrictEqual([
+            { 
+                questionTag: userPastViolatorQuestion.tag,  
+                answerTag: noPastViolator.tag
+            },
+            { 
+                questionTag: violatingQuestionWithEscalate.tag,  
+                answerTag: yesAnswerEscalate.tag
+            },
+        ]);
+        expect(nextQuestion).toStrictEqual(escalationQuestion);
+    })
+
     it('no next_question_tag - should throw', () => {
         expect(() => {
             handleNextClick(
@@ -59,6 +94,65 @@ describe('next click handler', () => {
                 basicDecisionTree,
                 violatingQuestionWithEscalate,
                 yesAnswerEscalate
+            )}
+        ).toThrow()
+    })
+});
+
+describe('back click handler', () => {
+    it('basic call', () => {
+        const { updatedQuestionsWithAnswers, prevQuestion } = handleBackClick(
+            [
+                { 
+                    questionTag: violatingQuestionWithEscalate.tag,
+                    answerTag: yesAnswerEscalate.tag 
+                },
+            ],
+            escDecisionTree,
+        );
+
+        expect(updatedQuestionsWithAnswers).toStrictEqual([]);
+        expect(prevQuestion).toStrictEqual(violatingQuestionWithEscalate);
+    })
+
+    it('includes multiple previous questions', () => {
+        const { updatedQuestionsWithAnswers, prevQuestion } = handleBackClick(
+            [
+                { 
+                    questionTag: userPastViolatorQuestion.tag,
+                    answerTag: noPastViolator.tag 
+                },
+                { 
+                    questionTag: violatingQuestionWithEscalate.tag,
+                    answerTag: yesAnswerEscalate.tag 
+                },
+            ],
+            pastViolatorDecisionTree,
+        );
+
+        expect(updatedQuestionsWithAnswers).toStrictEqual([
+            { 
+                questionTag: userPastViolatorQuestion.tag,  
+                answerTag: noPastViolator.tag
+            },
+        ]);
+        expect(prevQuestion).toStrictEqual(violatingQuestionWithEscalate);
+    })
+
+    it('no previous question - should throw', () => {
+        expect(() => {
+            handleBackClick(
+                [],
+                basicDecisionTree,
+            )}
+        ).toThrow()
+    })
+
+    it('previous question does not exist in tree - should throw', () => {
+        expect(() => {
+            handleBackClick(
+                [{ questionTag: userPastViolatorQuestion.tag, answerTag: noPastViolator.tag }],
+                escDecisionTree,
             )}
         ).toThrow()
     })
